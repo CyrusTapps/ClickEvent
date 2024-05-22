a9e1a3b49eb20f88ee99569dbd17ec6e6ae217da
Shawn Tapps <stapps.prsvr@gmail.com>
2024-05-21 21:02:31 -0500

    click assignment

---
+++ script.js
@@ -0,0 +1,35 @@
+let num=0;
+function count(){
+    
+num +=1;
+
+let upOne = document.getElementById("count")
+
+upOne.innerHTML = num;
+
+
+}
+
+
+
+function Alertme(){
+    alert("Death to Pokemon");
+}
+
+function printMe(){
+print("Do you wish to print that Pokemon is evial?")
+
+
+}
+
+
+function LogMe(){
+console.log("Pokemon is still terrible")
+
+
+}
+pokemon=background = "red";
+function yes(red){
+   
+    document.body.style.background = pokemon;
+}
